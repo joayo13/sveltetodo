0eb8eaa679d846b6f6ea9e9627a88c8d5d3a05e9
Jordan Ayotte <jordanayotte13@outlook.com>
2023-11-22 20:10:03 -0500

    added basic styles and each block for todos

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,13 +5,31 @@ function updateText(textChars) {
   text = textChars
 }
 function addToList() {
+  if(text)
     list = [...list, text]
     text = ''
 }
 </script>
-
-<h1>Todo App</h1>
-<h3>{text}</h3>
-<ul>{list}</ul>
-<input bind:value={text} type="text"/>
-<button on:click={addToList}>Add Todo</button>
+<style>
+  .app-container {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+  .todo-item {
+    padding: 1rem;
+    width: 12rem;
+    display: flex;
+    justify-content: space-between;
+  }
+</style>
+<section class="app-container">
+  <h1>Todo App With Svelte</h1>
+  <ul>
+    {#each list as item}
+    <li class="todo-item">{item}<button>Remove</button></li>
+    {/each}
+  </ul>
+  <input bind:value={text} type="text"/>
+  <button on:click={addToList}>Add Todo</button>
+</section>

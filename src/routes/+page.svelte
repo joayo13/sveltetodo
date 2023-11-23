f900277bd292031774ee8c2e2216c5a9d0b156cc
Jordan Ayotte <jordanayotte13@outlook.com>
2023-11-22 20:39:59 -0500

    can remove items from list, added time field

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script>
-let list = ['Make poop', 'Fart',]
+let list = []
 let text = ''
+let time = null
 function updateText(textChars) {
   text = textChars
 }
 function addToList() {
-  if(text)
-    list = [...list, text]
+  if(text && time) {
+    const item = { id: list.length, title: text, time: time}
+    list = [...list, item]
     text = ''
+    time = null
+  }
+}
+function removeFromList(item) {
+  list = list.filter((listItem) => listItem.id !== item.id)
 }
 </script>
 <style>
@@ -16,20 +23,37 @@ function addToList() {
     left: 50%;
     transform: translateX(-50%);
   }
+  .todo-list {
+    list-style-type: none;
+  }
   .todo-item {
     padding: 1rem;
     width: 12rem;
     display: flex;
-    justify-content: space-between;
+    flex-direction: column;
+  }
+  .todo-form {
+    padding: 1rem;
+    width: 12rem;
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
   }
 </style>
 <section class="app-container">
   <h1>Todo App With Svelte</h1>
-  <ul>
+  <ul class="todo-list">
     {#each list as item}
-    <li class="todo-item">{item}<button>Remove</button></li>
+    <li class="todo-item">
+      <p>{item.title}</p>
+      <p>{item.time}</p>
+      <button on:click={removeFromList(item)}>Remove</button>
+    </li>
     {/each}
   </ul>
-  <input bind:value={text} type="text"/>
-  <button on:click={addToList}>Add Todo</button>
+  <form on:submit={addToList} class="todo-form">
+  <input placeholder="Todo Title" bind:value={text} type="text"/>
+  <input bind:value={time} type="datetime-local"/>
+  <button>Add Todo</button>
+</form>
 </section>
